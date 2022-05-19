c0a3311b5e352bcbbaafa895d65a704999f7a112
Rabeya Khatun Muna <54771575+RabeyaMuna@users.noreply.github.com>
2022-05-19 15:56:52 +0600

    Update Don't give me five!.rb

--- Ruby/Codewar/Don't give me five!.rb
+++ Ruby/Codewar/Don't give me five!.rb
@@ -6,3 +6,17 @@ def dont_give_me_five(start_,end_)
     end
     return n      # n amount of numbers
 end
+
+OR
+
+def dont_give_me_five(start,end_)
+    (start..end_).count { | i | not i.to_s.include? '5' }
+end
+
+OR
+
+def dont_give_me_five(start_,end_)
+  (start_..end_)
+    .reject { |e| e.to_s.include?('5') }
+    .count
+end
