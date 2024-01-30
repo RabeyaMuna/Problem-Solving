332b032e9c05aec614b800674733c1d0d42ef44e
Rabeya Khatun Muna <54771575+RabeyaMuna@users.noreply.github.com>
2024-01-30 12:34:59 +0600

    Create 232.implement-queue-using-stacks.rb

---
+++ Data Structure in Ruby/Leetcode/232.implement-queue-using-stacks.rb
@@ -0,0 +1,47 @@
+class MyQueue
+    def initialize()
+      @arr = []
+    end
+
+
+=begin
+    :type x: Integer
+    :rtype: Void
+=end
+    def push(x)
+        @arr << x
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def pop()
+      @arr.shift
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def peek()
+        @arr[0]
+    end
+
+
+=begin
+    :rtype: Boolean
+=end
+    def empty()
+        @arr.empty?
+    end
+
+
+end
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue.new()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
