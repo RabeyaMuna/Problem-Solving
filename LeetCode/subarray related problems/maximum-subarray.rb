ce2d1b6af60ca9e0a7face95040e93546568c60d
Rabeya Khatun Muna <54771575+RabeyaMuna@users.noreply.github.com>
2024-03-13 12:12:29 +0600

    Update maximum-subarray.rb

--- LeetCode/subarray related problems/maximum-subarray.rb
+++ LeetCode/subarray related problems/maximum-subarray.rb
@@ -1,1 +1,14 @@
 
+# @param {Integer[]} nums
+# @return {Integer}
+def max_sub_array(nums)
+ current_max = nums[0]
+ max_sum = nums[0]
+
+ (1...nums.size).each do |i|
+   current_max = [nums[i], current_max + nums[i]].max
+   max_sum = [max_sum, current_max].max
+ end
+
+ return max_sum
+end
