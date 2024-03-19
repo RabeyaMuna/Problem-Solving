8866e85974702e39be64517184616e176a10e5e6
Rabeya Khatun Muna <54771575+RabeyaMuna@users.noreply.github.com>
2024-03-19 13:32:08 +0600

    Update 424.longest-repeating-character-replacement.rb

--- LeetCode/Window Sliding/424.longest-repeating-character-replacement.rb
+++ LeetCode/Window Sliding/424.longest-repeating-character-replacement.rb
@@ -1,29 +1,30 @@
 # I have initated hash
 # Add two pointers to point from same index
-#I have used window slicing concept 
+#I have used the window slicing concept 
+
 
 # @param {String} s
 # @param {Integer} k
 # @return {Integer}
+
 def character_replacement(s, k)
   hash_table = Hash.new(0)
   left = 0
   right = 0
   longest_rep = 0
-  max_key_value = 0
 
   while right < s.size
     hash_table[s[right]] += 1
-    max_key_value = [max_key_value, hash_table[s[right]]].max
 
-    while (right- left + 1) - max_key_value > k
-      left += 1
+    while (right- left + 1) - hash_table.values.max  > k
       hash_table[s[left]] -= 1
+      left += 1
     end
 
-      longest_rep = [longest_rep, right - left + 1].max
-      right += 1
+    longest_rep = [longest_rep, right - left + 1].max
+    right += 1
   end
    
    return longest_rep
 end
+
