a2d5a86841396a070e8c81aa29484000ea874423
Rabeya Khatun Muna <54771575+RabeyaMuna@users.noreply.github.com>
2024-03-20 19:43:08 +0600

    Create 105.construct-binary-tree-from-preorder-and-inorder-traversal.rb

---
+++ LeetCode/Binary Tree/105.construct-binary-tree-from-preorder-and-inorder-traversal.rb
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {Integer[]} preorder
+# @param {Integer[]} inorder
+# @return {TreeNode}
+def build_tree(preorder, inorder)
+  return if inorder.empty?
+
+  root_val = preorder.shift
+  root_index = inorder.index(root_val)
+
+  root = TreeNode.new(root_val)
+
+  root.left = build_tree(preorder, inorder[0...root_index])
+  root.right = build_tree(preorder, inorder[root_index+1...inorder.size])
+
+  root
+end
