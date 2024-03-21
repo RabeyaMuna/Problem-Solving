d05f7cbff01c1f274a2e47fd7c1ead4c61708309
Rabeya Khatun Muna <54771575+RabeyaMuna@users.noreply.github.com>
2024-03-22 00:15:31 +0600

    Create 213.house-robber-ii.rb

---
+++ LeetCode/213.house-robber-ii.rb
@@ -0,0 +1,27 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def rob(nums)
+  return nums[0] if nums.size == 1
+  return [nums[0], nums[1]].max if nums.size == 2
+
+  val1 = get_house_max(nums[0...nums.size-1])
+  val2 = get_house_max(nums[1...nums.size])
+
+  return [val1, val2].max
+end
+
+def get_house_max(nums)
+  return nums[0] if nums.size == 1
+  return [nums[0], nums[1]].max if nums.size == 2
+
+  dp = Array.new(nums.size, 0) 
+
+  dp[0] = nums[0]
+  dp[1] = [nums[0], nums[1]].max
+
+  (2...nums.size).each do |i|
+    dp[i]= [nums[i]+dp[i-2], dp[i-1]].max
+  end
+
+   return dp[nums.size-1]
+end
