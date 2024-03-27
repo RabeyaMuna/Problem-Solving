d0c9992dc5b65cf26db364da317aac0c37264720
Rabeya Khatun Muna <54771575+RabeyaMuna@users.noreply.github.com>
2024-03-27 12:19:01 +0600

    Update 1669.merge-in-between-linked-lists.rb

--- LeetCode/LinkList/1669.merge-in-between-linked-lists.rb
+++ LeetCode/LinkList/1669.merge-in-between-linked-lists.rb
@@ -40,3 +40,44 @@ def merge_in_between(list1, a, b, list2)
 
   return list1
 end
+
+
+
+==================================
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} list1
+# @param {Integer} a
+# @param {Integer} b
+# @param {ListNode} list2
+# @return {ListNode}
+def merge_in_between(list1, a, b, list2)
+  counter = 1
+  temp = list1
+  start_node = list1
+  end_node = list1
+
+  for i in (0..b) do
+    start_node = start_node.next if i < a - 1
+    end_node = end_node.next
+  end
+
+  start_node.next = list2
+
+  while list2
+    list2 = list2.next
+    start_node = start_node.next
+  end
+
+  start_node.next = end_node
+
+  return list1
+end
+
